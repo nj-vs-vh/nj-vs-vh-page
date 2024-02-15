8d252769a7ba45df090d5820b42f218ad53fa5dd
Igor Vaiman <igor.vaiman@inten.to>
2024-02-15 03:20:15 +0100

    basic project rendering

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,84 @@
 use askama::Template;
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    routing::get,
+    Router,
+};
+use std::{env, path};
 use tower_http::trace::TraceLayer;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
 mod project;
-mod template_resp;
 
-// #[tokio::main]
-// async fn main() {
-//     let subscriber = FmtSubscriber::builder()
-//         .with_max_level(Level::DEBUG)
-//         .finish();
-//     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+#[derive(Clone)]
+struct AppState {
+    project_catalog: project::ProjectCatalog,
+}
 
-//     let app = Router::new()
-//         .route("/", get(index))
-//         .layer(TraceLayer::new_for_http());
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-//     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-//         .await
-//         .unwrap();
+    let projects_dir = env::var("PROJECTS_DIR").unwrap_or("projects".to_owned());
+    tracing::info!("Reading project catalog from {}", &projects_dir);
+    let catalog_res = project::ProjectCatalog::load(path::Path::new(&projects_dir));
+    if let Err(e) = catalog_res {
+        tracing::error!("Failed to load project catalog: {}", e);
+        return;
+    }
+    let catalog = catalog_res.unwrap();
+    tracing::info!("Read project catalog: {}", &catalog);
 
-//     println!("listening on {}", listener.local_addr().unwrap());
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/project/:slug", get(project_page))
+        .layer(TraceLayer::new_for_http())
+        .with_state(AppState {
+            project_catalog: catalog,
+        });
 
-//     axum::serve(listener, app).await.unwrap();
-// }
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate {}
-
-async fn index() -> impl IntoResponse {
-    let template = IndexTemplate {};
-    template_resp::HtmlTemplateResponse(template)
+struct IndexTemplate {
+    number: usize, // temp
 }
 
-// TEMP
-fn main() {
-    env_logger::init();
-    let projects = project::Project::load_catalog(&std::path::Path::new("projects"));
-    println!("{:?}", projects);
+async fn index(State(state): State<AppState>) -> IndexTemplate {
+    IndexTemplate {
+        number: state.project_catalog.count(),
+    }
+}
+
+#[derive(Template)]
+#[template(path = "project.html")]
+struct ProjectTemplate {
+    title: String,
+    body_html: String,
+}
+
+async fn project_page(
+    State(state): State<AppState>,
+    Path(slug): Path<String>,
+) -> Result<ProjectTemplate, StatusCode> {
+    let project_match = state.project_catalog.find(&slug);
+    if let Some(project) = project_match {
+        Ok(ProjectTemplate {
+            title: project.metadata.title.clone(),
+            body_html: project.body_html.clone(),
+        })
+    } else {
+        Err(StatusCode::NOT_FOUND)
+    }
 }
--- src/project.rs
+++ src/project.rs
@@ -1,16 +1,20 @@
+use slugify::slugify;
 use std::{fs::File, io, path::Path};
 
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-struct Metadata {
-    title: String,
-    subtitle: Option<String>,
+#[derive(Deserialize, Debug, Clone)]
+pub struct Metadata {
+    pub title: String,
+    pub slug: Option<String>,
+    pub subtitle: Option<String>,
 }
 
+#[derive(Clone)]
 pub struct Project {
-    metadata: Metadata,
-    body: String,
+    pub metadata: Metadata,
+    body_md: String,
+    pub body_html: String,
 }
 
 impl std::fmt::Debug for Project {
@@ -18,7 +22,7 @@ impl std::fmt::Debug for Project {
         f.write_str(&format!(
             "Project{{ metadata: {:?}, body: <{} bytes> }}",
             self.metadata,
-            self.body.len()
+            self.body_md.len()
         ))
     }
 }
@@ -29,15 +33,51 @@ impl Project {
         if !dir.is_dir() {
             return Err(io::Error::other("path must be a directory".to_owned()));
         }
-        let metadata: Metadata = serde_yaml::from_reader(File::open(dir.join("meta.yaml"))?)
+        let mut metadata: Metadata = serde_yaml::from_reader(File::open(dir.join("meta.yaml"))?)
             .map_err(|e| io::Error::other(e.to_string()))?;
-        let body = std::io::read_to_string(File::open(dir.join("body.md"))?)?;
-        Ok(Project { metadata, body })
+        if metadata.slug == None {
+            metadata.slug = Some(slugify!(&metadata.title, max_length = 64));
+        }
+        let body_md = std::io::read_to_string(File::open(dir.join("body.md"))?)?;
+        let body_html = markdown::to_html(&body_md);
+        Ok(Project {
+            metadata,
+            body_md,
+            body_html,
+        })
     }
+    pub fn dummy() -> Project {
+        let body_md = "## Hello world\n\nThis is *markdown* **text**\n\n__cool!__".to_owned();
+        return Project {
+            metadata: Metadata {
+                title: "Project title".to_owned(),
+                slug: Some("example-project-slug".to_owned()),
+                subtitle: None,
+            },
+            body_html: markdown::to_html(&body_md),
+            body_md,
+        };
+    }
+}
 
-    pub fn load_catalog(projects_dir: &Path) -> io::Result<Vec<Project>> {
+#[derive(Debug, Clone)]
+pub struct ProjectCatalog {
+    projects: Vec<Project>,
+}
+
+impl std::fmt::Display for ProjectCatalog {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!(
+            "ProjectCatalog {{ {} items }}",
+            self.projects.len()
+        ))
+    }
+}
+
+impl ProjectCatalog {
+    pub fn load(projects_dir: &Path) -> io::Result<ProjectCatalog> {
         log::info!("loading project catalog from {:?}", projects_dir);
-        Ok(projects_dir
+        let projects: Vec<Project> = projects_dir
             .read_dir()?
             .filter_map(|maybe_dir_entry| {
                 if let Ok(entry) = maybe_dir_entry {
@@ -54,6 +94,30 @@ impl Project {
                 }
                 None
             })
-            .collect())
+            .collect();
+        let mut slugs: Vec<String> = projects
+            .iter()
+            .filter_map(|p| p.metadata.slug.clone())
+            .collect();
+        slugs.sort();
+        slugs.dedup();
+        if slugs.len() != projects.len() {
+            return Err(io::Error::other("Project catalog contains duplicate slugs"));
+        }
+        Ok(ProjectCatalog { projects })
+    }
+
+    pub fn count(&self) -> usize {
+        self.projects.len()
+    }
+
+    pub fn find(&self, slug: &str) -> Option<&Project> {
+        self.projects.iter().find(|&p| {
+            if let Some(project_slug) = p.metadata.slug.as_deref() {
+                project_slug == slug
+            } else {
+                false
+            }
+        })
     }
 }
